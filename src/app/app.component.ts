f4e441ab12a1027de03ac1592bf54ccc54dad99f
vaibahv bajpayee <vaibhavbajpayee2109@gmail.com>
2019-04-23 21:03:25 +0530

    events communiction added, life cycle hooks added, viewchild, contentview child added....basically all the things which help child parent communication has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'ng6-fulldive';
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
+  onMyNameCalled(myName: {name: string}) {
+    console.log('myname called', myName);
+  }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
 }
